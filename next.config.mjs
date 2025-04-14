e3a75a49814456e3f9240262776be4150231ee69
17ishan <iishansinhaa@gmail.com>
2025-04-15 01:28:50 +0530

    Delete all files from the repository

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  reactStrictMode: true,
-  // basePath: '/your-repo-name', // Uncomment & set if deploying to GitHub Pages
-};
-
-export default nextConfig;
